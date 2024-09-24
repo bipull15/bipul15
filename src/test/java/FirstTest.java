5e10b5556512776144e8054ba34a3fa884052c33
Hazard <hazard@gmail.com>
2024-09-24 03:22:31 -0500

    Initial commit with Maven Project and JUnit test cases

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void testAddition() {
+		// Test if 1 + 1 equals 2
+		assertEquals(2, 1 + 1);
+		
+	}
+	
+	@Test
+	public void testGreaterThan() {
+		// Test if 5 is greater than 2
+		assertTrue(5 > 2);
+	}
+	
+
+}
